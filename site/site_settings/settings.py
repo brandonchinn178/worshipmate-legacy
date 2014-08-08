54195f0c35e1859c9469b3d60ab68738afc85113
Brandon Chinn <brandonchinn178@berkeley.edu>
2014-08-07 19:27:29 -0700

    print static_root

--- site/site_settings/settings.py
+++ site/site_settings/settings.py
@@ -136,7 +136,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', '..', 'static')
+print os.path
 STATIC_URL = '/static/'
 
 #EMAIL_HOST = 'smtp.gmail.com'
