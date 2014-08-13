be6cb61bc5654b684b14321373d13363bfed2bdc
Brandon Chinn <brandonchinn178@berkeley.edu>
2014-08-13 00:44:46 -0700

    user/password

--- .openshift/action_hooks/addfulltext.py
+++ .openshift/action_hooks/addfulltext.py
@@ -1,7 +1,12 @@
 import MySQLdb, os
 
-conn = MySQLdb.connect(host=os.environ['OPENSHIFT_MYSQL_DB_HOST'],
-    db=os.environ['OPENSHIFT_APP_NAME'], port=int(os.environ['OPENSHIFT_MYSQL_DB_PORT']))
+conn = MySQLdb.connect(
+    host=os.environ['OPENSHIFT_MYSQL_DB_HOST'],
+    user=os.environ['OPENSHIFT_MYSQL_DB_USERNAME'],
+    password=os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'],
+    db=os.environ['OPENSHIFT_APP_NAME'],
+    port=int(os.environ['OPENSHIFT_MYSQL_DB_PORT'])
+)
 c = conn.cursor()
 
 c.execute('ALTER TABLE database_song ADD FULLTEXT(title, artist, themes, lyrics)')
