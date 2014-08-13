c599e406601af489babaa9d81642c922ac699413
Brandon Chinn <brandonchinn178@berkeley.edu>
2014-08-13 00:40:20 -0700

    changing addfulltext

--- .openshift/action_hooks/addfulltext.py
+++ .openshift/action_hooks/addfulltext.py
@@ -1,9 +1,9 @@
 import MySQLdb, os
 
-conn = MySQLdb.connect(host=os.environ['OPENSHIFT_MYSQL_DB_HOST'], user='brandon',
+conn = MySQLdb.connect(host=os.environ['OPENSHIFT_MYSQL_DB_HOST'],
     db=os.environ['OPENSHIFT_APP_NAME'], port=int(os.environ['OPENSHIFT_MYSQL_DB_PORT']))
 c = conn.cursor()
 
 c.execute('ALTER TABLE database_song ADD FULLTEXT(title, artist, themes, lyrics)')
-
+conn.commit()
 c.close()
