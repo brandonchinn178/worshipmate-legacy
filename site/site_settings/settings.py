a3540abddbb7966acbcbfa43a03f839cb25d2b6f
Brandon Chinn <brandonchinn178@berkeley.edu>
2014-09-21 22:13:34 -0700

    print BASE_DIR

--- site/site_settings/settings.py
+++ site/site_settings/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+print BASE_DIR
 STATIC_ROOT = os.path.join(BASE_DIR, '..', '..', 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, '..', 'static')]
