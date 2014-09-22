3ae1485bbb43b28c8150c85ebf2e7ba2532036e1
Brandon Chinn <brandonchinn178@berkeley.edu>
2014-09-21 22:37:29 -0700

    Removing collectstatic

--- site/site_settings/settings.py
+++ site/site_settings/settings.py
@@ -136,8 +136,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, '..', 'static')]
-
-if ON_OPENSHIFT:
-    STATIC_ROOT = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'site', 'static')
+STATIC_URL = '/static/'
