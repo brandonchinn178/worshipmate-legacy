3d94374ecbc91283ea5e5d664325797acd1f6870
Brandon Chinn <brandonchinn178@berkeley.edu>
2015-01-24 01:59:35 -0800

    Cleaning up

--- site/contact/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    url(r'^$', 'contact.views.index', name='index'),
-    url(r'^thanks/', 'contact.views.thanks')
-)
