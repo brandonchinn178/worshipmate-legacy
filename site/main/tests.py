b221adbd0621191e40c7a334efffb7c0b6ba0d76
Brandon Chinn <brandonchinn178@berkeley.edu>
2015-01-24 17:00:43 -0800

    Change name of backup

--- site/main/tests.py
+++ site/main/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.test.client import RequestFactory()
+from django.views.generic.base import TemplateView
 
-# Create your tests here.
+from main.views import add_title_mixin
+
+class WebsiteTest(TestCase):
+    def test_all_pages_exist(self):
+        for page in ['', 'about', 'contact', 'database', 'transpose']:
+            response = self.client.get('/%s' % page)
+            self.assertNotEqual(response, 500)
+
+class ViewsTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_add_title_mixin(self):
+        TestView = add_title_mixin(TemplateView)
+        setattr(TestView, 'title', 'Test Title')
+        view = TestView.as_view()
+        request = self.factory.get('/')
+        response = view(request)
+        self.assertEqual(response.context_data['title'], 'Test Title')
