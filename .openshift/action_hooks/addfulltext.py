965435071209c16a215ec63cb83d48557434ae15
Brandon Chinn <brandonchinn178@gmail.com>
2015-04-19 22:35:47 -0700

    comment out fulltext

--- .openshift/action_hooks/addfulltext.py
+++ .openshift/action_hooks/addfulltext.py
@@ -1,17 +1,17 @@
-import MySQLdb, os
+# import MySQLdb, os
 
-conn = MySQLdb.connect(
-    host=os.environ['OPENSHIFT_MYSQL_DB_HOST'],
-    user=os.environ['OPENSHIFT_MYSQL_DB_USERNAME'],
-    passwd=os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'],
-    db=os.environ['OPENSHIFT_APP_NAME'],
-    port=int(os.environ['OPENSHIFT_MYSQL_DB_PORT'])
-)
-c = conn.cursor()
+# conn = MySQLdb.connect(
+#     host=os.environ['OPENSHIFT_MYSQL_DB_HOST'],
+#     user=os.environ['OPENSHIFT_MYSQL_DB_USERNAME'],
+#     passwd=os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'],
+#     db=os.environ['OPENSHIFT_APP_NAME'],
+#     port=int(os.environ['OPENSHIFT_MYSQL_DB_PORT'])
+# )
+# c = conn.cursor()
 
-c.execute('ALTER TABLE database_song ADD FULLTEXT (title)')
-c.execute('ALTER TABLE database_song ADD FULLTEXT (artist)')
-c.execute('ALTER TABLE database_song ADD FULLTEXT (themes)')
-c.execute('ALTER TABLE database_song ADD FULLTEXT (lyrics)')
-conn.commit()
-conn.close()
+# c.execute('ALTER TABLE database_song ADD FULLTEXT (title)')
+# c.execute('ALTER TABLE database_song ADD FULLTEXT (artist)')
+# c.execute('ALTER TABLE database_song ADD FULLTEXT (themes)')
+# c.execute('ALTER TABLE database_song ADD FULLTEXT (lyrics)')
+# conn.commit()
+# conn.close()
