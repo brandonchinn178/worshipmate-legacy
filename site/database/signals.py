90d8f13637588e8cdc59e53e8a4eaad3c4046bc3
Brandon Chinn <brandonchinn178@gmail.com>
2015-10-11 22:55:17 -0700

    fix circle ci, add script to source .env

--- site/database/signals.py
+++ site/database/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 from django.template import Context
 from django.template.loader import get_template
+from django.conf import settings
 
 import facebook, os
 
@@ -12,7 +13,7 @@ def post_to_facebook(sender, instance, created, **kwargs):
     """
     When a Song is added to the database, post to Facebook automatically.
     """
-    if created:
+    if created and not settings.ON_CI:
         access_token = os.environ.get('FACEBOOK_ACCESS_TOKEN')
         context = Context({'song': instance})
         message = get_template('facebook_post.txt').render(context)
