4c91896f81a30e27add9b800ebf8dcdbd9240f0b
Brandon Chinn <brandonchinn178@gmail.com>
2016-05-31 23:50:45 -0700

    finish admin index page

--- site/admin/views.py
+++ site/admin/views.py
@@ -4,4 +4,18 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from database.models import Song, Theme
 
 class MainView(LoginRequiredMixin, TemplateView):
+    template_name = 'admin/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        context['songs'] = Song.objects.order_by('title')
+        return context
+
+class AddSongView(LoginRequiredMixin, TemplateView):
+    pass
+
+class EditSongView(LoginRequiredMixin, TemplateView):
+    pass
+
+class AccountView(LoginRequiredMixin, TemplateView):
     pass
