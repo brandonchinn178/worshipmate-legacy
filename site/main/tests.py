7936c514f27d4d1c94c049f6861e06883c11ae58
Brandon Chinn <brandonchinn178@gmail.com>
2016-06-02 10:30:12 -0700

    remove tests

--- site/main/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-class WebsiteTest(TestCase):
-    def test_all_pages_exist(self):
-        for page in ['', 'about', 'contact']:
-            response = self.client.get('/%s' % page)
-            self.assertNotEqual(response, 500)
