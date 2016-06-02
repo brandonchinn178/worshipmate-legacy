20c8f4ecba30ba001c2f7b5bf2cdbd7ee34b6981
Brandon Chinn <brandonchinn178@gmail.com>
2016-06-01 22:34:41 -0700

    get rid of slug test

--- site/database/tests.py
+++ site/database/tests.py
@@ -10,16 +10,3 @@ class SongTest(TestCase):
             IntegrityError,
             Song.objects.create
         )
-
-    def test_slug(self):
-        song = Song.objects.create(title='Test Title')
-        self.assertEqual(song.title_slug, '')
-        self.assertEqual(song.get_absolute_url(), '/song/test-title/')
-        # calling get_absolute_url should set title_slug
-        self.assertEqual(song.title_slug, 'test-title')
-
-        song = Song.objects.create(title="Yet Another Test", title_slug="welll")
-        # if has title_slug, nothing changes
-        self.assertEqual(song.title_slug, 'welll')
-        self.assertEqual(song.get_absolute_url(), '/song/welll/')
-        self.assertEqual(song.title_slug, 'welll')
