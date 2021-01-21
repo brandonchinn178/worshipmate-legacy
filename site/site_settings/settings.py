6ac8b2183b8d277102e6412e358929e04d88f616
Brandon Chinn <brandonchinn178@gmail.com>
2021-01-20 19:41:54 -0800

    Disable emails

--- site/site_settings/settings.py
+++ site/site_settings/settings.py
@@ -124,11 +124,11 @@ if IS_HEROKU:
     DEFAULT_FILE_STORAGE = 'main.custom_storages.MediaStorage'
 
     # email settings
-    EMAIL_HOST = 'smtp.sendgrid.net'
-    EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-    EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
+    #EMAIL_HOST = 'smtp.sendgrid.net'
+    #EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+    #EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+    #EMAIL_PORT = 587
+    #EMAIL_USE_TLS = True
 
 LOGIN_REDIRECT_URL = 'admin:index'
 LOGIN_URL = 'admin:login'
